421a40f3af4d53a67451259eed2d6078fb6cbfc8
101ex <asdf>
2017-11-29 14:21:20 +0100

    connected database to news template

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Apparel(models.Model):
+
+    apparel_name=models.Charfield(max_length=255)
+    apparel_image=models.CharField(max_length=1000)
+    apparel_brand=models.CharField(max_length=255)
+    apparel_size=models.CharField(max_length=255)
+
+    def __str__(self):
+        return
